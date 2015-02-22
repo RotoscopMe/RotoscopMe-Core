976eee34483f5e0951ca8a487ebf0d63f99320f0
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2015-02-22 21:11:37 +0100

    Use exception

--- projet.cpp
+++ projet.cpp
@@ -4,15 +4,33 @@
 
 Projet::Projet(QString &name, QDir &workspace, QFile &video, int frequence)
 {
-    if(workspace.exists() && video.exists() && workspace.mkdir(name))
+    if(workspace.exists())
     {
-        _project = new QDir(workspace.path()+"/"+name);
-        qDebug() << _project->path();
-        _project->mkdir("input");
-        video.copy(_project->path()+"/input/video");
-        _video = new QFile(_project->path()+"/input/video");
+        if(video.exists())
+        {
+            if(workspace.mkdir(name))
+            {
+                _project = new QDir(workspace.path()+"/"+name);
+                qDebug() << _project->path();
+                _project->mkdir("input");
+                video.copy(_project->path()+"/input/video");
+                _video = new QFile(_project->path()+"/input/video");
 
-        QProcess process;
-        process.startDetached("avconv -i " + _project->path() +"/input/video -vsync 1 -r 8 -y " + _project->path() + "/input/img%d.jpg");
+                QProcess process;
+                process.startDetached("avconv -i " + _project->path() +"/input/video -vsync 1 -r 8 -y " + _project->path() + "/input/img%d.jpg");
+            }
+            else
+            {
+                throw QString("Ce projet existe déjà");
+            }
+        }
+        else
+        {
+            throw QString("La vidéo n'existe pas");
+        }
+    }
+    else
+    {
+        throw QString("Le workspace n'existe pas");
     }
 }
